0acffca3fdcbcc06d9894ec44d8ef591bcbae8c4
francesca <francesca.cannata32@gmail.com>
2026-06-09 16:44:06 +0200

    fix useful functions

--- files/Example Codes/functions.py
+++ files/Example Codes/functions.py
@@ -14,26 +14,26 @@ def barron_func(c, omega, x, phi):
 
 
 # Piecewise function
-def piecewise_func(x):
-    x_points = np.linspace(0, 1, 15)
-    y_points = np.random.random(len(x_points))
-    y_points[0] = 0             # the first point is equal to 0
-    y_points[-1] = 1            # the last point is equal to 1
+def piecewise_func(x, seed=0):
+    np.random.seed(seed)
+    x_points = np.array([0, np.random.rand(), 1])
+    y_points = np.random.randn(len(x_points))
     pw_func = np.interp(x, x_points, y_points)  # It computes the 1D piecewise linear interpolant to a function with given discrete data points (x_points, y_points), evaluated at x
     return pw_func
 
 
 # Define the hat functions considering the boundary conditions
 def hat_function(x, J, M):
+    return np.maximum(0, 1 - np.abs(M * x - J))
     # we need to define the boundary conditions
-    if J == 0:
+    #if J == 0:
         # if x <= 1/M:              # if x[i] is in the left side (i.e. between 0 and s_1 = 1/m), we want just a downward segment \
         #     return 1 - M*x
         # elif x >= 1 - 1/M:        # if x[i] is in the right side (i.e. between s_{m-1} and s_m = 1), we want just a upwards segment /
         #     return 1 + M*(x-1)
         # else:                        # if x[i] is between s_1 = 1/m and s_{m-1} then the hat function is zero
         #     return 0
-        return np.maximum(0,np.maximum(1 - M*x,1 + M*(x-1)))
+        #return np.maximum(0,np.maximum(1 - M*x,1 + M*(x-1)))
     # elif 0 < J < M - 1:      # inner points (s_1, ..., s_{m-1})
     #     if S[J-1] < x <= S[J+1]:
     #         return 1 - np.abs(M*x - J)
@@ -44,5 +44,5 @@ def hat_function(x, J, M):
     #         return 1 - np.abs(M * x - J)
     #     else:
     #         return 0
-    else:
-        return np.maximum(0, 1 - np.abs(M * x - J))
+    #else:
+        #return np.maximum(0, 1 - np.abs(M * x - J))
